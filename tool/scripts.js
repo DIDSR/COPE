96fea4c5eee9be56819789cd2ff3e88c12423f6a
Chumin Zhao <104772230+chuminzhao@users.noreply.github.com>
2024-12-05 11:02:05 -0500

    Update scripts.js

--- tool/scripts.js
+++ tool/scripts.js
@@ -528,8 +528,8 @@ async function newTrial(response) {
             contrast: contrast,
             frequency: Math.round(frequency*26*100)/100,
             max_freq: max_freq*26,
-            size_std: std/10,
-            position: position,
+            //size_std: std/10,
+            position_xyz (m): position,
             trialTime: stimulusOff - stimulusOn,
       });
     }
